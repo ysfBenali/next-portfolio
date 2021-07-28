77c004da9fa4394e99601dc8fa8d27a773795c3a
Youssef BenAli <youyssef@gmail.com>
2021-07-28 20:58:30 +0100

    Update Projects module + Fetch top 8 starred repositories

--- src/components/common/Main/index.jsx
+++ src/components/common/Main/index.jsx
@@ -1,12 +1,14 @@
 import Intro from '@/components/modules/Intro';
+import Projects from '@/components/modules/Projects';
 import { useTheme } from 'providers/ThemeProvider';
 import { Wrapper } from './styles';
 
-const Main = () => {
+const Main = ({ data }) => {
   const theme = useTheme();
   return (
     <Wrapper theme={theme}>
       <Intro />
+      <Projects repos={data}/>
     </Wrapper>
   );
 };
