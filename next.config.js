92b66e5cbef636ae7fddf82aa82d5850d1dd9554
ybenali <youssef.benali@digimind.com>
2023-01-26 22:22:41 +0100

    fix: update withPWA config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
-const withPWA = require("next-pwa");
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
 
 module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-  },
+  // next.js config
 });
